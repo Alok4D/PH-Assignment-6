db64bdeb0c87d247ecac6c06d528269eff721301
Alok4D <alokroy602701@gmail.com>
2024-03-04 23:42:44 +0600

    add new commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
+
+const newMenu = document.getElementById("title-menu");
 const discussPostContainer = async (searchText, isLoading) => {
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`
-  );
+  const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
   const data = await res.json();
   const dataAll = data.posts;
   console.log(dataAll);
@@ -26,7 +26,7 @@ const letDiscuss = (dataAll) => {
         <div class="p-10">
           <figure class="absolute ">
             <div class="pl-14">
-                <div class="absolute w-[15px] rounded-2xl bg-red-600 h-[15px]"></div>
+                <div class="absolute w-[15px] rounded-2xl ${data.isActive? "bg-green-600":"bg-red-600"} h-[15px]"></div>
             </div>
             <img src="${data.image}" class=" bg-white rounded-2xl border-red-600 h-[72px] w-[72px] "/>
           </figure>
@@ -68,7 +68,8 @@ const letDiscuss = (dataAll) => {
                         <div class="font-bold text-[#12132D99] text-[16px]">${data.posted_time} min</div>
                     </div>
                 </div>
-                <button class="text-[#10B981]">
+
+                <button onclick="myData('${data.title}',${data.view_count})" class="text-[#10B981]">
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
                         <g clip-path="url(#clip0_57_425)">
                           <path d="M13.9998 0C6.26805 0 9.15527e-05 6.26814 9.15527e-05 13.9999C9.15527e-05 21.7314 6.26805 28 13.9998 28C21.7315 28 27.9999 21.7314 27.9999 13.9999C27.9999 6.26814 21.7315 0 13.9998 0ZM14 4.91741L22.2847 10.0835H5.71542L14 4.91741ZM22.3879 18.333H22.3871C22.3871 19.1616 21.7155 19.8331 20.887 19.8331H7.1131C6.28447 19.8331 5.61303 19.1615 5.61303 18.333V10.4122C5.61303 10.3245 5.62199 10.2393 5.63655 10.1556L13.552 15.0914C13.5617 15.0975 13.5721 15.1016 13.5821 15.1072C13.5925 15.113 13.6032 15.1186 13.6138 15.1239C13.6697 15.1527 13.7273 15.176 13.7862 15.1912C13.7923 15.1929 13.7983 15.1936 13.8044 15.195C13.869 15.2102 13.9344 15.2197 13.9998 15.2197H14.0002C14.0007 15.2197 14.0012 15.2197 14.0012 15.2197C14.0665 15.2197 14.1319 15.2105 14.1965 15.195C14.2026 15.1935 14.2086 15.1929 14.2147 15.1912C14.2735 15.176 14.3309 15.1527 14.3871 15.1239C14.3977 15.1186 14.4084 15.113 14.4188 15.1072C14.4287 15.1016 14.4392 15.0975 14.4489 15.0914L22.3644 10.1556C22.3789 10.2393 22.3879 10.3244 22.3879 10.4122V18.333Z" fill="#10B981"/>
@@ -80,6 +81,7 @@ const letDiscuss = (dataAll) => {
                         </defs>
                       </svg>
                 </button>
+
             </div>
           </div>
         </div>
@@ -90,11 +92,42 @@ const letDiscuss = (dataAll) => {
 
         `;
 
+     
+ 
+
     //
     cardContainer.appendChild(humanCard);
   });
   toggleLoadingSpinner(false);
+
+
 };
+
+let num = 0;
+const myData = (titel, view) => {
+  num++
+  document.getElementById("newNumber").innerHTML = num;
+  const div = document.createElement('div');
+      div.classList.add("newPostss");
+      div.innerHTML = `
+      <div class="flex justify-between">
+      <h3>${titel}</h3>
+    <div>
+      <div class="flex gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+          <path d="M11.6667 14C11.6667 14.6188 11.9125 15.2123 12.3501 15.6499C12.7877 16.0875 13.3812 16.3333 14 16.3333C14.6188 16.3333 15.2123 16.0875 15.6499 15.6499C16.0875 15.2123 16.3333 14.6188 16.3333 14C16.3333 13.3812 16.0875 12.7877 15.6499 12.3501C15.2123 11.9125 14.6188 11.6667 14 11.6667C13.3812 11.6667 12.7877 11.9125 12.3501 12.3501C11.9125 12.7877 11.6667 13.3812 11.6667 14Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M24.5 14C21.7 18.6667 18.2 21 14 21C9.8 21 6.3 18.6667 3.5 14C6.3 9.33333 9.8 7 14 7C18.2 7 21.7 9.33333 24.5 14Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <p>${view}</p>
+      </div>
+    </div>
+      
+     
+    </div>
+      `;
+      newMenu.appendChild(div);
+}
+
 
 // handle search button
 const handleSearch = () => {
@@ -112,14 +145,14 @@ const handleSearch = () => {
 const toggleLoadingSpinner = (isLoading) => {
   const loadingSpinner = document.getElementById("loading-spinner");
   loadingSpinner.classList.remove('hidden');
-
-
   if (isLoading) {
     loadingSpinner.classList.remove("hidden");
   } else {
     loadingSpinner.classList.add("hidden");
   }
-
 };
 
-// discussContainer();
+//
+
+
+discussContainer();
